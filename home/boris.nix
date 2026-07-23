79cf4747a87d596f54f718730a26e9829e76a0c3
Boris Schwarz <schwarz_boris@hotmail.com>
2026-07-23 20:04:16 +0200

    feat: add home-manager, manage git per user

---
+++ home/boris.nix
@@ -0,0 +1,10 @@
+{...}:{
+	home.username = "boris";
+	home.homeDirectory = "/home/boris";
+	home.stateVersion = "26.05";
+	programs.git = {
+		enable = true;
+		userName = "Boris Schwarz";
+		userEmail = "schwarz_boris@hotmail.com";
+	};
+}
