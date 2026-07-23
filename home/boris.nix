13862ead9340a73f563056d525df3dff475da3dc
Boris Schwarz <schwarz_boris@hotmail.com>
2026-07-23 20:46:59 +0200

    add shell aliases

--- home/boris.nix
+++ home/boris.nix
@@ -7,4 +7,11 @@
 		userName = "Boris Schwarz";
 		userEmail = "schwarz_boris@hotmail.com";
 	};
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			nix-rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#t15";
+			nix-test = "sudo nixos-rebuild test --flake ~/nixos-config#t15";
+		};
+	};
 }
