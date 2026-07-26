b08184762fdee0dfa07744ff81c592b9ceb8f611
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-07-26 19:00:29 +0200

    add autocomplete to neovim

--- home/common/neovim.nix
+++ home/common/neovim.nix
@@ -167,6 +167,23 @@
               vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count =  1, float = true }) end, opts)
             end,
           })
+
+          -- LSP completion
+          vim.o.completeopt = "menuone,noselect,popup,fuzzy"
+
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client and client:supports_method("textDocument/completion") then
+                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+              end
+            end,
+          })
+
+          -- Manually open the menu with Ctrl-Space
+          vim.keymap.set("i", "<C-Space>", function()
+            vim.lsp.completion.get()
+          end)
         '';
       }
     ];
