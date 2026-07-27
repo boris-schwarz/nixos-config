8fed0951a59ca4f9c89b816d4d4a1745fb172eff
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-07-27 09:50:17 +0200

    add extensions to firefox

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,5 +7,26 @@
   ];
 
   # programs with their own nixos module
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    preferences = {
+      "extensions.allowPrivateBrowsingByDefault" = true;
+    };
+    policies.ExtensionSettings = {
+      # Bitwarden Password Manager
+      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+        installation_mode = "force_installed";
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        default_area = "navbar";
+        private_browsing = false;
+      };
+      # uBlock Origin
+      "uBlock0@raymondhill.net" = {
+        installation_mode = "force_installed";
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        default_area = "navbar";
+        private_browsing = true;
+      };
+    };
+  };
 }
