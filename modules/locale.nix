e5a42db168a47743ba1c807a9f9416d513af1598
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-07-27 17:11:23 +0200

    change localization settings

--- modules/locale.nix
+++ modules/locale.nix
@@ -2,4 +2,15 @@
   time.timeZone = "Europe/Zurich";
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "sg"; # keyboard layout for text console before GUI starts
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "de_CH.UTF-8/UTF-8"
+  ];
+  i18n.extraLocaleSettings = {
+    LC_TIME = "de_CH.UTF-8";
+    LC_NUMERIC = "de_CH.UTF-8";
+    LC_MONETARY = "de_CH.UTF-8";
+    LC_PAPER = "de_CH.UTF-8";
+    LC_MEASUREMENT = "de_CH.UTF-8";
+  };
 }
