1277a109e51079f2602c8637d0e3f65127ab2cef
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-07-28 12:44:18 +0200

    rework custom nix-commands

--- home/common/bash.nix
+++ home/common/bash.nix
@@ -8,12 +8,12 @@
     };
     initExtra = ''
       nix-rebuild() {
-        echo "+ sudo nixos-rebuild switch --flake ~/nixos-config#t15"
-        sudo nixos-rebuild switch --flake ~/nixos-config#t15
+        echo "+ sudo nixos-rebuild switch --flake ~/nixos-config#t15 $*"
+        sudo nixos-rebuild switch --flake ~/nixos-config#t15 "$@"
       }
       nix-test() {
-        echo "+ sudo nixos-rebuild test --flake ~/nixos-config#t15"
-        sudo nixos-rebuild test --flake ~/nixos-config#t15
+        echo "+ sudo nixos-rebuild test --flake ~/nixos-config#t15 $*"
+        sudo nixos-rebuild test --flake ~/nixos-config#t15 "$@"
       }
     '';
   };
