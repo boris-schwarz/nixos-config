382bdd964b77e90de4c98c0124ae187520747ce2
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-07-28 15:03:08 +0200

    add automatic garbage collection

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,9 +1,24 @@
-{ ... }:
+{
+  ...
+}:
 
 {
+  # enable flakes and the new nix-command
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
-  ]; # enable flakes and the new nix-command
-  nixpkgs.config.allowUnfree = true; # allow unfree package installation
+  ];
+
+  # allow unfree package installation
+  nixpkgs.config.allowUnfree = true;
+
+  # automated garbage-collection of old generations
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  # dedupe identical files in the store (hardlinks)
+  nix.settings.auto-optimise-store = true;
 }
