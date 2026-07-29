dad6a5b51b3dfd1fde100f93f0647e269c87abcf
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-07-29 12:58:50 +0200

    add bash alias for zeditor to map to zed

--- home/common/bash.nix
+++ home/common/bash.nix
@@ -5,6 +5,7 @@
     enable = true;
     shellAliases = {
       nix-edit = "nvim ~/nixos-config";
+      zed = "zeditor"; # nixpkgs zed is the ZFS Event Daemon, probably not gonna use it ever, so reclaiming the zed command
     };
     initExtra = ''
       nix-rebuild() {
