e7e42c4893f9f3596bf4f5f2296b167a3998247b
Boris Schwarz <13311628+boris-schwarz@users.noreply.github.com>
2026-09-22 15:55:43 +0200

    add roboto font

--- home/common/fonts.nix
+++ home/common/fonts.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
+  home.packages = [
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.roboto
+  ];
   fonts.fontconfig.enable = true;
 }
